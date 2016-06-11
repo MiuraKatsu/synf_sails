f9048cf4a7c4add48316de6dcbec502742dc4cb6
MiuraKatsu <MiuraKatsu@gmail.com>
2016-06-11 16:39:00 +0900

    ユーザと家族と冷蔵庫

---
+++ api/controllers/FridgeController.js
@@ -0,0 +1,11 @@
+/**
+ * FridgeController
+ *
+ * @description :: Server-side logic for managing fridges
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
